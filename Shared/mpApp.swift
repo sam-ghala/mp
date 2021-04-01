6895928f84c1326474b5b6f1b70ab54c60b1cc2b
samghala <ghalayini.sj@gmail.com>
2021-03-31 22:43:44 -0400

    Initial Commit

---
+++ Shared/mpApp.swift
@@ -0,0 +1,17 @@
+//
+//  mpApp.swift
+//  Shared
+//
+//  Created by Sam Ghalayini on 3/31/21.
+//
+
+import SwiftUI
+
+@main
+struct mpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
